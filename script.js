b17425a08461ae5a4f38983226d70fc41f1f831a
rera25 <arvinfe723@gmail.com>
2024-09-03 16:26:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm_password').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        event.preventDefault();
+    }
+});
